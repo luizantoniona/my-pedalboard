a03c44d84a185e82d3cb73faa8d211c3537c1314
luizantoniona <luizantonio.na@hotmail.com>
2025-05-19 14:10:16 -0300

    fix: Adjust build for qt5 and portaudio

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ int main( int argc, char* argv[] ) {
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for ( const QString& locale : uiLanguages ) {
         const QString baseName = "my-pedalboard_" + QLocale( locale ).name();
+
+        qInfo() << "Installing translation for:" << baseName;
         if ( translator.load( ":/translations/" + baseName ) ) {
             app.installTranslator( &translator );
             break;
@@ -18,15 +20,15 @@ int main( int argc, char* argv[] ) {
     }
 
     QQmlApplicationEngine engine;
-    const QUrl url( QStringLiteral( "qrc:/main.qml" ) );
+    const QUrl url( QStringLiteral( "qrc:/qml/main.qml" ) );
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreated,
         &app,
         [ url ]( QObject* obj, const QUrl& objUrl ) {
-            if ( !obj && url == objUrl )
-                QCoreApplication::exit( -1 );
-        },
+        if ( !obj && url == objUrl )
+            QCoreApplication::exit( -1 );
+    },
         Qt::QueuedConnection );
     engine.load( url );
 
