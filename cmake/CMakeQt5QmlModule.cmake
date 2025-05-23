b6daf163ecc7323a90821df165178d94e6dbe80e
luizantoniona <luizantonio.na@hotmail.com>
2025-05-23 11:08:21 -0300

    chore: correcting qt5 run

--- cmake/CMakeQt5QmlModule.cmake
+++ cmake/CMakeQt5QmlModule.cmake
@@ -3,7 +3,7 @@ message(STATUS "Configuring: ${MODULE_NAME} With: Qt5QML")
 file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/qml/${MODULE_NAME})
 
 foreach(qml_file ${QML_FILE_LIST})
-    configure_file(${qml_file} ${CMAKE_BINARY_DIR}/qml/${MODULE_NAME}/ COPYONLY)
+    configure_file(${qml_file} ${CMAKE_BINARY_DIR}/qml/${MODULE_NAME}/${qml_file} COPYONLY)
 endforeach()
 
 configure_file(qmldir ${CMAKE_BINARY_DIR}/qml/${MODULE_NAME}/qmldir COPYONLY)
