cd89e002597fefd5b68d6196f0295a63d0497804
luizantoniona <luizantonio.na@hotmail.com>
2025-06-06 17:21:36 -0300

    feat: New functions for declaring designs controls

--- Core/RegisterTypes.cpp
+++ Core/RegisterTypes.cpp
@@ -2,12 +2,26 @@
 
 #include <QQmlEngine>
 
-#include "audio/AudioManager.h"
-
+#include "Audio/AudioManager.h"
+#include "Molecules/NavigationBarControl.h"
 #include "Pages/HomePageControl.h"
 
 void RegisterTypes::registerTypes() {
 
+    registerControls();
+
+}
+
+void RegisterTypes::registerControls() {
+
+    // Register Atoms Controls
+
+    // Register Molecules Controls
+    qmlRegisterType<NavigationBarControl>( "Molecules", 1, 0, "NavigationBarControl" );
+
+    // Register Organisms Controls
+
     // Register Pages Controls
     qmlRegisterType<HomePageControl>( "Pages", 1, 0, "HomePageControl" );
+
 }
--- Core/RegisterTypes.h
+++ Core/RegisterTypes.h
@@ -4,6 +4,9 @@
 class RegisterTypes {
 public:
     static void registerTypes();
+
+private:
+    static void registerControls();
 };
 
 #endif // REGISTERTYPES_H
